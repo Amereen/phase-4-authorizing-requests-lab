f4bd027743d12f9c4a907d0736fb6fc87edddeb1
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-09-01 20:09:35 -0400

    commit tree 19d3dff607f22c63133716d6b2c57cd50e5a156e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-
+# def user_signed_in?
+#   session[:user_id].present?
+# end
 end
--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,14 +1,26 @@
 class MembersOnlyArticlesController < ApplicationController
+  before_action :authenticate_user, except:[:index,:show]
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
+    if user_signed_in?
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
+    else
+      render json: {error: 'Not authorized'}, status: :unauthorized
+    end
+  
   end
 
   def show
+    if user_signed_in?
     article = Article.find(params[:id])
     render json: article
+    else
+      render json: {error:"Not authorized"}, status: :unauthorized
+    end
+  
   end
 
   private
@@ -16,5 +28,7 @@ class MembersOnlyArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
+def user_signed_in?
+  session[:user_id].present?
+end
 end
